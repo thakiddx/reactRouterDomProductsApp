fb771090d0f4f3e287933438b2462a6be90301b8
Xavier Malone <xmalone0920@gmail.com>
2023-09-20 16:01:18 -0700

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ function App() {
     <Router>
       <div>
         <Navbar />
+        <Footer />
       </div>
       </Router>
   ); 
---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import <p></p> from 'react-router-dom';
+
+function Footer() {
+    return (
+        <footer>
+            <p>Copyright information here</p>
+        </footer>
+    );
+}
+
+export default Footer;
