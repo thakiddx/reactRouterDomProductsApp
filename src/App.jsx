9f23ba8f9790fcb88c008113d65e9ecac957ad63
Xavier Malone <xmalone0920@gmail.com>
2023-09-20 15:40:14 -0700

    navbar

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { BrowserRouter as Router } from 'react-router-dom';
+import Navbar from './components/Navbar'
+
+function App() {
+
+  return (
+    <Router>
+      <div>
+        <Navbar />
+      </div>
+      </Router>
+  ); 
+}
+
+export default App
