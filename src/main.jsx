b9f6f3cf0e61f48d4e63382f23118d879fae977a
thakiddx <xmalone0920@gmail.com>
2023-10-05 08:37:20 -0700

    the final commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
-//import BrowserRouter from react-router-dom
-//Use the BrowserRouter to wrap the App component
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom'; 
 import App from './App.jsx';
 import './index.css';
+import './App.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
